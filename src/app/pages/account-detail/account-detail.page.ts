e3bcef6eebc95961d8f5a8b78211c806b9e4f812
Isaac Yriarte <isaacyagi@gmail.com>
2021-05-30 00:40:24 -0500

    Feat: Implementa generacion de codigo QR

--- src/app/pages/account-detail/account-detail.page.ts
+++ src/app/pages/account-detail/account-detail.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { BarcodeScanner } from '@ionic-native/barcode-scanner/ngx';
 
 @Component({
   selector: 'app-account-detail',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AccountDetailPage implements OnInit {
 
-  constructor() { }
+  constructor(
+    private barcodeScanner: BarcodeScanner
+  ) { }
 
   ngOnInit() {
   }
 
+  async getQRCode(){
+    console.log("Generating qr code");
+    const resultGenerate = await this.barcodeScanner.encode(this.barcodeScanner.Encode.TEXT_TYPE,"Test generating barcode");
+    console.log("YAGI - resultGenerate - resultGenerate=",resultGenerate);
+    console.log("YAGI - resultGenerate - resultGenerate (JSON)=",JSON.stringify(resultGenerate));
+  }
 }
