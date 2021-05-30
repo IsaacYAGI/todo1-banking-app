d2c10962335eb2406b8c9d2725d7386b910532af
Isaac Yriarte <isaacyagi@gmail.com>
2021-05-30 17:50:33 -0500

    Feat: Genera el codigo QR para la cuenta seleccionada

--- src/app/pages/account-detail/account-detail.page.ts
+++ src/app/pages/account-detail/account-detail.page.ts
@@ -37,8 +37,14 @@ export class AccountDetailPage implements OnInit {
 
   async getQRCode(){
     console.log("Generating qr code");
-    const resultGenerate = await this.barcodeScanner.encode(this.barcodeScanner.Encode.TEXT_TYPE,"Test generating barcode");
-    console.log("YAGI - resultGenerate - resultGenerate=",resultGenerate);
-    console.log("YAGI - resultGenerate - resultGenerate (JSON)=",JSON.stringify(resultGenerate));
+    const qrContent = [
+      this.customerService.customerData.email,
+      this.accountDetail.account_number,
+      this.customerService.customerData.name,
+      this.customerService.customerData.lastname,
+    ].join(";");
+    const resultGenerate = await this.barcodeScanner.encode(this.barcodeScanner.Encode.TEXT_TYPE, qrContent);
+    // console.log("YAGI - resultGenerate - resultGenerate=",resultGenerate);
+    // console.log("YAGI - resultGenerate - resultGenerate (JSON)=",JSON.stringify(resultGenerate));
   }
 }
