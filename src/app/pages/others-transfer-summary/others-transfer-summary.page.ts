2c1a119babdb0e9da09f163ff57d031c6a196703
Isaac Yriarte <isaacyagi@gmail.com>
2021-05-30 19:31:15 -0500

    Feat: Muestra el resultado de la transferencia

--- src/app/pages/others-transfer-summary/others-transfer-summary.page.ts
+++ src/app/pages/others-transfer-summary/others-transfer-summary.page.ts
@@ -1,21 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { AccountsService } from 'src/app/services/accounts.service';
 
 @Component({
   selector: 'app-others-transfer-summary',
   templateUrl: './others-transfer-summary.page.html',
   styleUrls: ['./others-transfer-summary.page.scss'],
 })
-export class OthersTransferSummaryPage implements OnInit {
+export class OthersTransferSummaryPage implements OnInit, OnDestroy {
+
+  destinationData: any = null;
+  lastTransferData: any = null;
 
   constructor(
     private router: Router,
+    public accountsService: AccountsService,
+
   ) { }
 
   ngOnInit() {
+
+    this.destinationData = this.accountsService.selectedDestinationAccount;
+    this.lastTransferData = this.accountsService.lastTransferOperation;
   }
 
   backToHome(){
     this.router.navigateByUrl("/home");
+  }
+
+  ngOnDestroy(){
+    console.log("Cleaning scanned account");
+    this.accountsService.cleanSelectedAccount();
   }
 }
